349a90b23f293cd738d327a0c76cf3f2b1c2d8f6
Rafael Haone <haone95@gmail.com>
2023-09-23 14:19:42 -0300

    Troca de imagens por animação.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
-const maxRecords = 1200
+const maxRecords = 151
 const limit = 10
 let offset = 0
 
@@ -14,7 +14,8 @@ function loadPokemonItens(offset, limit) {
 
                 <div class="detail">
                     <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`)
+                    .join('')}
                     </ol>
 
                     <img src="${pokemon.photo}" 
